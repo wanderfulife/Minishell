3d5b6e9f8df319a3caf8e9230a5700089843013a
jcohen <jcohen@student.42.fr>
2024-12-30 18:06:35 +0100

    136 full erros handling messages done

--- src/executor/executor_child.c
+++ src/executor/executor_child.c
@@ -6,7 +6,7 @@
 /*   By: jcohen <jcohen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 10:00:00 by JoWander          #+#    #+#             */
-/*   Updated: 2024/12/30 17:03:08 by jcohen           ###   ########.fr       */
+/*   Updated: 2024/12/30 18:01:56 by jcohen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,50 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/stat.h>
+
+static int	is_directory(const char *path)
+{
+	struct stat	statbuf;
+
+	if (stat(path, &statbuf) != 0)
+		return (0);
+	return (S_ISDIR(statbuf.st_mode));
+}
 
 static void	handle_command_not_found(char *cmd)
 {
-	ft_putstr_fd("minishell: command not found: ", 2);
-	ft_putendl_fd(cmd, 2);
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putendl_fd(": command not found", 2);
 	exit(127);
 }
 
-static void	handle_execution_failed(char *cmd, char *path)
+static void	handle_execution_error(char *cmd, char *path)
 {
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(cmd, 2);
+	ft_putstr_fd(": ", 2);
+	if (access(cmd, F_OK) != 0)
+	{
+		ft_putendl_fd("No such file or directory", 2);
+		free(path);
+		exit(127);
+	}
+	else if (access(cmd, X_OK) != 0)
+	{
+		ft_putendl_fd("Permission denied", 2);
+		free(path);
+		exit(126);
+	}
+	else if (is_directory(cmd))
+	{
+		ft_putendl_fd("Is a directory", 2);
+		free(path);
+		exit(126);
+	}
+	ft_putendl_fd("Error executing command", 2);
 	free(path);
-	ft_putstr_fd("minishell: execution failed: ", 2);
-	ft_putendl_fd(cmd, 2);
 	exit(126);
 }
 
@@ -51,7 +82,7 @@ void	executor_child_process(t_command *cmd, t_shell *shell)
 	if (!path)
 		handle_command_not_found(cmd->args[0]);
 	execve(path, cmd->args, shell->env);
-	handle_execution_failed(cmd->args[0], path);
+	handle_execution_error(cmd->args[0], path);
 }
 
 void	executor_exit_status(int status, t_shell *shell)
